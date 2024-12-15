ffc2827126a14fcffb5e86f28b0661299510aff1
Ashton <dinhhai1ccg@gmail.com>
2024-12-15 17:29:47 +0000

    Modified parameters in extract_frames.sh

--- scripts/extract_frames.sh
+++ scripts/extract_frames.sh
@@ -3,5 +3,5 @@
 python -m utils.extract_frames --input_folder INPUT_FOLDER \
                                --output_base_folder OUTPUT_BASE_FOLDER
 
-python -m utils.check_num_frames --base_path BASE_PATH \
+python -m utils.check_num_frames --base_path OUTPUT_BASE_FOLDER \
                                  --n 25
