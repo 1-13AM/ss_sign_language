c2584da9c07add816ad945464b4d720d3c3fb6d1
Ashton <dinhhai1ccg@gmail.com>
2024-12-15 17:26:54 +0000

    Modified extract_frames.py file

--- scripts/extract_frames.sh
+++ scripts/extract_frames.sh
@@ -1,6 +1,7 @@
 # scripts to extract frames from videos
 
-python -m utils.extract_frames --input_folder INPUT_FOLDER
+python -m utils.extract_frames --input_folder INPUT_FOLDER \
                                --output_base_folder OUTPUT_BASE_FOLDER
 
-python -m utils.check_num_frames --base_path OUTPUT_BASE_FOLDER
+python -m utils.check_num_frames --base_path BASE_PATH \
+                                 --n 25
